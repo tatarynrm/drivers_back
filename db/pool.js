f3abfc3433cd4e4f139779468e2946aaa9d54bbc
tatarynrm <tatarynrm@gmail.com>
2023-10-27 15:19:08 +0300

    eqwewqewq

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,20 @@
+// const pool = {
+//   user: process.env.DB_LOGIN_NAME,
+//   password: process.env.DB_LOGIN_PASSWORD,
+//   connectString: process.env.DB_LOGIN_CONNECT_STRING,
+//   poolMax: Number(process.env.DB_POOL_MAX || 20),
+//   poolMin: Number(process.env.DB_POOL_MIN || 10),
+//   poolIncrement: Number(process.env.DB_POOL_INCREMENT || 1),
+//   poolTimeOut: Number(process.env.DB_POOL_TIMEOUT || 60),
+// };
 const pool = {
-  user: process.env.DB_LOGIN_NAME,
-  password: process.env.DB_LOGIN_PASSWORD,
-  connectString: process.env.DB_LOGIN_CONNECT_STRING,
-  poolMax: Number(process.env.DB_POOL_MAX || 20),
-  poolMin: Number(process.env.DB_POOL_MIN || 10),
-  poolIncrement: Number(process.env.DB_POOL_INCREMENT || 1),
-  poolTimeOut: Number(process.env.DB_POOL_TIMEOUT || 60),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  connectString: process.env.DB_CONNECTION_URI,
+  // poolMax: Number(process.env.DB_POOL_MAX || 20),
+  // poolMin: Number(process.env.DB_POOL_MIN || 10),
+  // poolIncrement: Number(process.env.DB_POOL_INCREMENT || 1),
+  // poolTimeOut: Number(process.env.DB_POOL_TIMEOUT || 60),
 };
 
 module.exports = pool;
